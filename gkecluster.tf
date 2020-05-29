5697b013e7ef6d52155a6da2b70cbd3c818ddcd6
Ricardo <rherrera@nisum.com>
2020-05-29 14:47:10 -0400

    fixing deployment

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "gke-cluster" {
-    name               = "nodejsapi-cluster"
+    name               = "v23ppl"
     network            = "default"
-    location           = "southamerica-east1"
+    location           = "southamerica-east1-a"
     initial_node_count = 3
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  project     = "ppl-278520"
-  region      = "southamerica-east1"
-  zone        = "southamerica-east1-a"
+  project     = "test23ppl"
+  region      = "southamerica-east1-a"
 }
